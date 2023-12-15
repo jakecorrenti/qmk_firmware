eed8bfc635349b789bfdcfa15ef43b1993193373
Jake Correnti <jakecorrenti+github@proton.me>
2023-12-14 22:44:52 -0500

    working callum layout
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- keyboards/boardsource/microdox/keymaps/jakecorrenti/keymap.c
+++ keyboards/boardsource/microdox/keymaps/jakecorrenti/keymap.c
@@ -68,21 +68,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_LBRC, KC_LCBR, KC_LPRN, KC_TILD,      KC_CIRC, KC_RPRN, KC_RCBR, KC_RBRC, KC_GRV,
     KC_MINS, KC_ASTR, KC_EQL,  KC_UNDS, KC_DLR,       KC_HASH, OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,
     KC_PLUS, KC_PIPE, KC_AT,   KC_BSLS, KC_PERC,      XXXXXXX, KC_AMPR, KC_SCLN, KC_COLN, KC_EXLM,
-                      XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX
+                      XXXXXXX, _______, XXXXXXX,      XXXXXXX, _______, XXXXXXX
   ),
 
   [NAV] = LAYOUT_split_3x5_3(
     KC_TAB,  SW_WIN,  TABL,    TABR,    KC_VOLU,      KC_CAPS, HOME,    KC_UP,   END,     KC_DEL,
     OS_CMD,  OS_ALT,  OS_CTRL, OS_SHFT, KC_VOLD,      KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, QK_BOOT,
     SPCL,    SPC_R,   BACK,    FWD,     KC_MPLY,      KC_ENT,  KC_PGDN, KC_PGUP, SW_LANG, XXXXXXX,
-                      XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX
+                      XXXXXXX, _______, XXXXXXX,      XXXXXXX, _______, XXXXXXX
   ),
 
   [NUM] = LAYOUT_split_3x5_3(
     XXXXXXX, KC_7,    KC_8,    KC_9,    XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_4,    KC_5,    KC_6,    KC_0,         XXXXXXX, OS_SHFT, OS_CTRL, OS_ALT,  OS_CMD,
     XXXXXXX, KC_1,    KC_2,    KC_3,    XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                      XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX
+                      XXXXXXX, _______, XXXXXXX,      XXXXXXX, _______, XXXXXXX
   )
 };
 
